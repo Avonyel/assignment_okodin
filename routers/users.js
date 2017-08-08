e2289298849233aab592306723c5ca714d61fcd7
Stephanie Barker <song_mage@yahoo.com>
2017-08-07 20:54:17 -0400

    think there's something wrong with id incrementation in the findOrCreate, maybe

--- routers/users.js
+++ routers/users.js
@@ -27,51 +27,61 @@ router.post("/new", (req, res) => {
 	console.log(`coordinate = ${randomCoordinate}`);
 	sequelize
 		.transaction(t => {
-			return User.create({
-				username: req.body.username,
-				email: req.body.email,
-				profile_id: null,
-				transaction: t
-			})
+			return User.create(
+				{
+					username: req.body.username,
+					email: req.body.email,
+					profile_id: null
+				},
+				{ transaction: t }
+			)
 				.then(createdUser => {
 					//spread????, create location later
 					user = createdUser;
-					return Profile.create({
-						about: req.body.about,
-						talents: req.body.talents,
-						favorite_things: req.body.favoriteThings,
-						why: req.body.why,
-						user_id: user.id,
-						age: age,
-						location_id: null,
-						gender: req.body.gender,
-						relationship_status: req.body.Relationship,
-						education: req.body.education,
-						kids: req.body.kids,
-						height: height,
-						occupation: req.body.occupation,
-						transaction: t
-					});
+					return Profile.create(
+						{
+							about: req.body.about,
+							talents: req.body.talents,
+							favorite_things: req.body.favoriteThings,
+							why: req.body.why,
+							user_id: user.id,
+							age: age,
+							location_id: null,
+							gender: req.body.gender,
+							relationship_status: req.body.Relationship,
+							education: req.body.education,
+							kids: req.body.kids,
+							height: height,
+							occupation: req.body.occupation
+						},
+						{ transaction: t }
+					);
 				})
 				.then(createdProfile => {
 					//create location coordinate
 					profile = createdProfile;
 					return Location.findOrCreate({
-						defaults: { coordinate: 6 },
+						defaults: { coordinate: 9 },
 						where: { name: req.body.location },
 						transaction: t
 					}).spread(createdLocation => {
 						//update user
+						console.log("LOCATION HERE HEYO");
 						location = createdLocation;
 						return Profile.update(
-							{ location_id: location.id },
+							{ location_id: createdLocation.id },
 							{ where: { id: profile.id }, transaction: t }
-						).then(createdProfile => {
-							return User.update(
-								{ profile_id: createdProfile.id },
-								{ where: { id: user.id }, transaction: t }
-							);
-						});
+						)
+							.then(createdProfile => {
+								console.log("FINAL STEP HERE HEYO");
+								return User.update(
+									{ profile_id: createdProfile.id },
+									{ where: { id: user.id }, transaction: t }
+								);
+							})
+							.then(() => {
+								res.redirect("/");
+							});
 					});
 				});
 		})
